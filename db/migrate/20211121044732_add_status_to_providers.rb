ce9fa27ae737e8b52acfc4f65c25d72a5ee79af7
wakki0202 <ytk0202@outlook.jp>
2021-11-21 14:58:43 +0900

    案件完了機能＆口座情報追加

---
+++ db/migrate/20211121044732_add_status_to_providers.rb
@@ -0,0 +1,5 @@
+class AddStatusToProviders < ActiveRecord::Migration[6.1]
+  def change
+    add_column :providers, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_023331) do
+ActiveRecord::Schema.define(version: 2021_11_21_044732) do
 
   create_table "introductions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_023331) do
     t.string "appeal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "users", force: :cascade do |t|
