357d2715fa8551d6e54db77583090ab579436c29
wakki0202 <ytk0202@outlook.jp>
2021-12-23 02:06:12 +0900

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('jquery')
+//import Rails from "@rails/ujs"
+//import Turbolinks from "turbolinks"
+//import * as ActiveStorage from "@rails/activestorage"
+//require("channels")
+//require('jquery')
 
 Rails.start()
 Turbolinks.start()
