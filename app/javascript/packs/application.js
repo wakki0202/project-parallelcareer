46bc63a673bdd8f9dea7dbd480d9ede16673caf2
wakki0202 <ytk0202@outlook.jp>
2022-01-05 15:23:24 +0900

    popper add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require('jquery')
-require('popper')
-require('bootstrap-sprockets')
+
 
 Rails.start()
 Turbolinks.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,4 +6,14 @@ environment.plugins.prepend('Provide',
         jQuery: 'jquery/src/jquery'
     })
 )
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Popper: 'popper.js'
+    })
+)
 module.exports = environment
