5e9409e6acaef5273546efcc8dc5f7ffd400c956
wakki0202 <ytk0202@outlook.jp>
2022-01-05 15:17:39 +0900

    modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require('jquery')
+require('popper')
+require('bootstrap-sprockets')
 
 Rails.start()
 Turbolinks.start()
