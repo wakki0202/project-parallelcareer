9480f972464f1bfacba89bf55ed7ed42fe47b7f3
wakki0202 <ytk0202@outlook.jp>
2022-01-05 15:35:49 +0900

    application.js fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require('jquery')
 import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 
 Rails.start()
 Turbolinks.start()
