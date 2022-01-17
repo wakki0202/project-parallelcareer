d193ea8b730d42faae46abd9614f3ac141dbe7f6
wakki0202 <ytk0202@outlook.jp>
2022-01-17 20:25:42 +0900

    AWS fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,11 +8,12 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'parallelcareer' # バケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/project-parallelcareer'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AKIAXZG2CHZAWO2HJWX2'], # アクセスキー
-      aws_secret_access_key: ENV['Oz9rAaSa2rdGR3+zHDmBem6Vk3h/WfNMH9MtqE1e'], # シークレットアクセスキー
+      aws_access_key_id: ENV[:AKIAXZG2CHZAWO2HJWX2], # アクセスキー
+      aws_secret_access_key: ENV[:Oz9rAaSa2rdGR3+zHDmBem6Vk3h/WfNMH9MtqE1e], # シークレットアクセスキー
       region: 'ap-northeast-1', # リージョン
       path_style: true
     }
