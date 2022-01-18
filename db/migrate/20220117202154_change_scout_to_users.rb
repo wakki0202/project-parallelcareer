cc83552f96f28e9356674e7e18715d1870935484
wakki0202 <ytk0202@outlook.jp>
2022-01-18 22:07:35 +0900

    change date scout

--- db/migrate/20220117202154_change_scout_to_users.rb
+++ db/migrate/20220117202154_change_scout_to_users.rb
@@ -1,5 +1,5 @@
 class ChangeScoutToUsers < ActiveRecord::Migration[6.1]
   def change
-    change_column :users, :scout, :boolean
+    change_column :users, :scout, 'boolean USING CAST(users AS scout)'
   end
 end
