3da4a837eccf55ad77cf998251994908ecba1423
wakki0202 <ytk0202@outlook.jp>
2022-02-02 12:58:54 +0900

    top topfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when User
-      tops_index_path
+      users_mypage_path
     when Provider
       works_path
 
