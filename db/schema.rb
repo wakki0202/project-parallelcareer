00c569ad924dc0b3845580db494c55ebaea097a1
wakki0202 <ytk0202@outlook.jp>
2022-02-04 18:47:35 +0900

    companynameadd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_053646) do
+ActiveRecord::Schema.define(version: 2022_02_04_094556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 2022_02_03_053646) do
     t.string "link"
     t.string "images"
     t.integer "provider_id"
+    t.string "company_name"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
