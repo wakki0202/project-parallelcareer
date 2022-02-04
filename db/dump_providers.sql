c0e47f0df82af4fa48af547943973458bdcf6129
wakki0202 <ytk0202@outlook.jp>
2022-02-04 18:29:14 +0900

    works add fix

---
+++ db/dump_providers.sql
@@ -0,0 +1,31 @@
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (1,'田中 瑶介','tanachu_78@yahoo.co.jp','$2y$10$IYcEiXk4AKR.MMsHCppLLuy1304ADCKTJ5pNHkkp20D3SznJKScxa','2020-11-05 05:39:45','2020-11-26 03:31:59');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (13,'永間直人','nagamanaoto@gmail.com','$2y$10$KC9YVyjIj7XiBanfP12hcuPOFDcotkDQHJMTuPs020YVs0Q6oHSyu','2020-11-26 03:44:05','2020-11-26 03:44:05');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (19,'株式会社ノーガード','taibenjaminkuzuhara@gmail.com','$2y$10$cn2gTQHmqkqSLKjjceGpj.GfUppYCot2fuZlv/UVug7/A6z1IQh7G','2021-01-15 00:30:00','2021-01-15 00:30:00');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (20,'randxdy@gmail.com','randxdy@gmail.com','$2y$10$t6klfzOKwRpzqyw.gWwhKeQeNmvS.YYgb8yaDcLXLF5gaKL2OFe8C','2021-01-19 20:06:47','2021-01-19 20:06:47');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (21,'randxdy@gmail.com','randxdy@gmail.com','$2y$10$h66tFJC0wehyLrw383JKYuS.vJTdlmKIy6lF6PPZv02m8nT1H6tVi','2021-01-19 20:07:51','2021-01-19 20:07:51');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (23,'永間祐介','nagamantus@gmail.com','$2y$10$O7SbaCQGvtyp0c4t5QG8NumwRKCutcEPNYsJquiZuK1fDDiDKgBCK','2021-01-22 03:58:17','2021-01-22 03:58:17');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (25,'橋本尚武','n.hashimoto@nncom.co.jp','$2y$10$caXdOPVqLyPyNy9RtEbcXee/J02vAffPaQnIX9pPwqmhW2HMg5Vpu','2021-01-25 00:21:16','2021-08-13 01:41:28');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (26,'株式会社MTS','nagamantus@gmail.com','$2y$10$m8Rew5kF4v8HXCXtcrMu2e3l1s7rOBulqeFhDUDzEiAAhGS0JfF82','2021-01-25 20:17:49','2021-01-25 20:17:49');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (27,'合同会社企業団体支援機構','sensui623@gmail.com','$2y$10$znSWPo1FZ07PHHuwDpckreAB.aqW/XC153L0RLTVTuVwEfMzt3F9K','2021-01-25 20:29:56','2021-01-25 20:29:56');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (28,'avalue.create@gmail.com','avalue.create@gmail.com','$2y$10$rXkZAOAF35ZEEMSTDVXSG.RBP7YYm1lnyzC/cZ8BPi8oM/7JNe9zy','2021-01-26 00:43:07','2021-01-26 00:43:07');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (29,'MTS','nagamantus@gmail.com','$2y$10$WUz0LUxu8e5JpleaYTZ6RuJmWlim45lXCvqc8DkT49N/0hTLu70eW','2021-02-03 04:51:27','2021-02-03 04:51:27');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (30,'MTS','nagamantus@gmail.com','$2y$10$3V4rIFBDHA9cVGpvJheJ1.xMG6Am7wf/sEMyjsx8N/nXg4W5GH35K','2021-02-03 04:56:10','2021-02-03 04:56:10');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (31,'株式会社MTS','nagamantus@gmail.com','$2y$10$wf/zfbejKjL/6m0wrsgLQON5g7frw01UmJySMvcecbRWIAim0AnvK','2021-02-03 04:58:25','2021-02-03 04:58:25');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (32,'永間','nagamantus+1@gmail.com','$2y$10$QBrqAMqxrG9S29v4D3CTiuc9JQMgB329GefG3KBA42ZzNPu.0gD6u','2021-02-03 05:00:17','2021-02-03 05:00:17');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (33,'株式会社お菓子共和国','okashi.kyouwakoku.ami@gmail.com','$2y$10$pF/hnLVpjHUEOhjVmr6vFe1zG.vKKCN5RyYMmu3nVcTd3ObD/J/Ti','2021-02-08 18:14:34','2021-02-08 18:14:34');
+
+INSERT INTO providers (id, name, email, encrypted_password, created_at, updated_at) VALUES (40,'スタイリー不動産','shota.yoshida@let-inc.net','$2y$10$i7h.72DbJizgOJeaw7ovl.5EBvCo2sp6YKT/.ijHTz5FrxqIxUbey','2021-02-25 19:27:45','2021-03-07 18:13:41');
