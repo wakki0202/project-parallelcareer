2988d95bdf89b0cadff33635259cfda2a82337a5
wakki0202 <ytk0202@outlook.jp>
2022-02-06 14:30:36 +0900

    count

--- app/controllers/providers/invitations_controller.rb
+++ app/controllers/providers/invitations_controller.rb
@@ -2,8 +2,10 @@ class Providers::InvitationsController < Devise::InvitationsController
   def new
     
     @providers = Provider.all
-    @introductionnumber = Introduction.where(step: nil).count
-    @detailnumber = Detail.where(status: "未対応").count
+    @introductionnumber = current_provider.works.joins(:introductions).where(introductions: {step: nil}).count
+    @introductionall = Introduction.where(step: nil).count
+    @detailnumber = current_provider.works.joins(:details).where(details: {status: "未対応"}).count
+    @detailnumberall = Detail.where(status: "未対応").count
     super
   end
 
