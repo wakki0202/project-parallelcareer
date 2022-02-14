5bd0f56acfedd6f742006408f9e363846aa75f94
wakki0202 <ytk0202@outlook.jp>
2022-02-14 13:06:22 +0900

    works fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,4 +22,19 @@ $(function () {
   });
 });
 
- 
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    $(".slide").slick({
+      arrows: false, //左右の矢印はなし
+      autoplay: true, //自動的に動き出すか。初期値はfalse。
+      autoplaySpeed: 50, //自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+      speed: 6900, //スライドのスピード。初期値は300。
+      pauseOnHover: false, //オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+      pauseOnFocus: false, //フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+      cssEase: "linear", //動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+      slidesToShow: 4, //スライドを画面に4枚見せる
+      slidesToScroll: 1, //1回のスライドで動かす要素数
+      variableWidth: true,
+    });
+  });
+});
