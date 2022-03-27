12f539c755c02a57ff3611d6259004a60b9de47b
wakki0202 <ytk0202@outlook.jp>
2022-03-27 12:36:23 +0900

    csv add invited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,14 +71,16 @@ require 'csv'
     # CSV.generateとは、対象データを自動的にCSV形式に変換してくれるCSVライブラリの一種
     csv_data = CSV.generate(bom) do |csv|
       # %w()は、空白で区切って配列を返します
-      column_names = %w(ID 作成日 名前 電話番号 eメール 出身 生年月日 ラインID 銀行 支店 口座種類 口座番号  経歴 アピールポイント 招待者➀ 招待者➁ 招待者電話番号➀ 招待者電話番号➁ 招待者eメール➀ 招待者メール➁ 退会日)
+      column_names = %w(ID 作成日 退会日 名前 電話番号 eメール 出身 生年月日 ラインID 銀行 支店 口座種類 口座番号  経歴 アピールポイント 招待者➀ 招待者電話番号➀ 招待者eメール➀ 招待者➁  招待者電話番号➁ 招待者メール➁ )
       # csv << column_namesは表の列に入る名前を定義します。
       csv << column_names
       # column_valuesに代入するカラム値を定義します。
       users.each do |user|
+        if user.invited_by.present?
         column_values = [
           user.id,
           user.created_at,
+          user.deleted_at,
           user.name,
           user.tel,
           user.email,
@@ -92,16 +94,16 @@ require 'csv'
           user.career,
           user.appeal,
           user.invitername,
-          user.invited_by.name,
           user.inviteremail,
-          user.invited_by.email,
           user.invitertel,
-          user.invited_by.tel,
-          user.deleted_at
-
+          user.invited_by.name,          
+          user.invited_by.email,          
+          user.invited_by.tel        
                 ]
+              
       # csv << column_valueshは表の行に入る値を定義します。
         csv << column_values
+      end
       end
     end
     # csv出力のファイル名を定義します。
