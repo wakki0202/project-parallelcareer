7ea06bdd105fff861e6c270bbae2341eea66bc24
wakki0202 <ytk0202@outlook.jp>
2022-03-27 12:27:03 +0900

    csv fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ require 'csv'
     # CSV.generateとは、対象データを自動的にCSV形式に変換してくれるCSVライブラリの一種
     csv_data = CSV.generate(bom) do |csv|
       # %w()は、空白で区切って配列を返します
-      column_names = %w(ID 作成日 名前 電話番号 eメール 出身 生年月日 ラインID 銀行 支店 口座種類 口座番号  経歴 アピールポイント 招待者 招待者電話番号 招待者eメール 退会日)
+      column_names = %w(ID 作成日 名前 電話番号 eメール 出身 生年月日 ラインID 銀行 支店 口座種類 口座番号  経歴 アピールポイント 招待者➀ 招待者➁ 招待者電話番号➀ 招待者電話番号➁ 招待者eメール➀ 招待者メール➁ 退会日)
       # csv << column_namesは表の列に入る名前を定義します。
       csv << column_names
       # column_valuesに代入するカラム値を定義します。
@@ -91,9 +91,12 @@ require 'csv'
           user.account_no,
           user.career,
           user.appeal,
-          user.invitername.or(user.invited_by.name) ,
-          user.inviteremail.or(user.invited_by.email),
-          user.invitertel.or(user.invited_by.tel),
+          user.invitername,
+          user.invited_by.name,
+          user.inviteremail,
+          user.invited_by.email,
+          user.invitertel,
+          user.invited_by.tel,
           user.deleted_at
 
                 ]
